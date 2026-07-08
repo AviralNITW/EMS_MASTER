b038cdd46ef111e447e77844f71577573b0688b9
AviralNITW <maviral456@gmail.com>
2026-07-08 15:45:38 +0530

    fix: remove BrowserRouter basename for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/EMS_MASTER">
+    <BrowserRouter>
       <AuthProvider>
         <ThemeProvider>
           <App />
